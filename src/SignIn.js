c076bf1718dd29e7a107218ded595d18be50330b
Miki Biba <mikibiba4@gmail.com>
2022-05-20 00:45:48 +0300

    navigating from loginPage

--- src/SignIn.js
+++ src/SignIn.js
@@ -2,34 +2,25 @@ import * as React from 'react';
 import Button from '@mui/material/Button';
 import firebase from './firebase';
 import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth"
-
-const auth = getAuth(firebase);
-
-const provider = new GoogleAuthProvider();
-
-const signInWithGoogle = () => {
-    signInWithPopup(auth, provider).then((result) => {
-        const name = result.user.displayName;
-        const email = result.user.email;
-        const profilePic = result.user.photoURL;
-        localStorage.setItem("name", name);
-        localStorage.setItem("email", email);
-        localStorage.setItem("profilePic", profilePic);
-    })
-}
+import { useNavigate } from 'react-router-dom';
 
 export default function SignIn() {
+    const auth = getAuth(firebase);
+    const navigate = useNavigate();
+    const provider = new GoogleAuthProvider();
+
     return (
         <div>
             <Button
-                onClick={signInWithGoogle}
+                onClick={async () => {
+                    localStorage.setItem("user", JSON.stringify((await signInWithPopup(auth, provider)).user))
+                    navigate("/app")
+                }}
                 type="submit"
                 variant="contained"
-                sx={{ mt: 2, mb: 1 }}
-            >
+                sx={{ mt: 2, mb: 1 }} >
                 Sign In With Google
             </Button>
-
             <h5>{localStorage.getItem("name")}</h5>
             <h5>{localStorage.getItem("email")}</h5>
         </div >
