c0d1b3f939e7099bb63b746b72d0ebcf0259e032
Alex Biba <alexbiba@gmail.com>
2022-05-25 13:59:45 -0400

    Remove some boxes

--- src/App.js
+++ src/App.js
@@ -16,7 +16,6 @@ import History from './History';
 
 import {
     AppBar,
-    Box,
     Divider,
     Drawer,
     IconButton,
@@ -44,73 +43,66 @@ export default function App() {
 
     return (
         <DialogProvider>
-            <Box>
-                <AppBar>
-                    <Toolbar>
-                        <IconButton onClick={() => setOpenDrawer(!openDrawer)}><MenuIcon /></IconButton>
-                        <Typography>Goal Buddy</Typography>
-                    </Toolbar>
-                </AppBar>
-                <Toolbar />
-                <Box>
-                    <Drawer
-                        open={openDrawer}
-                        onClose={() => setOpenDrawer(!openDrawer)}
-                    >
-                        <List>
-                            {[
-                                ['My Goals', <LibraryBooksIcon />, 'my-goals'],
-                                ['Buddy Goals', <LibraryAddCheckIcon />, 'buddy-goals'],
-                                ['Set Goals', <LibraryAddIcon />, 'set-goals'],
-                                ['History', <HistoryIcon />, 'history'],
-                            ].map(([text, icon, route], index) => (
-                                <ListItem key={index} disablePadding>
-                                    <ListItemButton onClick={() => {
-                                        setOpenDrawer(!openDrawer);
-                                        navigate(route);
-                                    }}>
-                                        <ListItemIcon>{icon}</ListItemIcon>
-                                        <ListItemText primary={text} />
-                                    </ListItemButton>
-                                </ListItem>
-                            ))}
-                        </List>
-                        <Divider />
-                        <List>
-                            {[
-                                ['Settings', <SettingsIcon />, 'settings'],
-                                ['Sign Out', <LogoutIcon />, 'sign-in']
-                            ].map(([text, icon, route], index) => (
-                                <ListItem key={index} disablePadding>
-                                    <ListItemButton onClick={() => {
-                                        setOpenDrawer(!openDrawer);
-                                        navigate(route);
-                                    }}>
-                                        <ListItemIcon>{icon}</ListItemIcon>
-                                        <ListItemText primary={text} />
-                                    </ListItemButton>
-                                </ListItem>
-                            ))}
-                        </List>
-                    </Drawer>
-                </Box>
-                <Routes>
-                    <Route
-                        path="/"
-                        element={(
-                            JSON.parse(localStorage.getItem('user')).email ?
-                                <Navigate to="/my-goals" /> :
-                                <Navigate to="/sign-in" />
-                        )}
-                    />
-                    <Route path='sign-in' element={<SignIn />} />
-                    <Route path='my-goals' element={<MyGoals />} />
-                    <Route path='buddy-goals' element={<BuddyGoals />} />
-                    <Route path='set-goals' element={<SetGoals />} />
-                    <Route path='history' element={<History />} />
-                    <Route path='settings' element={<Settings />} />
-                </Routes>
-            </Box>
+            <AppBar>
+                <Toolbar>
+                    <IconButton onClick={() => setOpenDrawer(!openDrawer)}><MenuIcon /></IconButton>
+                    <Typography>Goal Buddy</Typography>
+                </Toolbar>
+            </AppBar>
+            <Toolbar />
+            <Drawer open={openDrawer} onClose={() => setOpenDrawer(!openDrawer)}>
+                <List>
+                    {[
+                        ['My Goals', <LibraryBooksIcon />, 'my-goals'],
+                        ['Buddy Goals', <LibraryAddCheckIcon />, 'buddy-goals'],
+                        ['Set Goals', <LibraryAddIcon />, 'set-goals'],
+                        ['History', <HistoryIcon />, 'history'],
+                    ].map(([text, icon, route], index) => (
+                        <ListItem key={index} disablePadding>
+                            <ListItemButton onClick={() => {
+                                setOpenDrawer(!openDrawer);
+                                navigate(route);
+                            }}>
+                                <ListItemIcon>{icon}</ListItemIcon>
+                                <ListItemText primary={text} />
+                            </ListItemButton>
+                        </ListItem>
+                    ))}
+                </List>
+                <Divider />
+                <List>
+                    {[
+                        ['Settings', <SettingsIcon />, 'settings'],
+                        ['Sign Out', <LogoutIcon />, 'sign-in']
+                    ].map(([text, icon, route], index) => (
+                        <ListItem key={index} disablePadding>
+                            <ListItemButton onClick={() => {
+                                setOpenDrawer(!openDrawer);
+                                navigate(route);
+                            }}>
+                                <ListItemIcon>{icon}</ListItemIcon>
+                                <ListItemText primary={text} />
+                            </ListItemButton>
+                        </ListItem>
+                    ))}
+                </List>
+            </Drawer>
+            <Routes>
+                <Route
+                    path="/"
+                    element={(
+                        JSON.parse(localStorage.getItem('user')).email ?
+                            <Navigate to="/my-goals" /> :
+                            <Navigate to="/sign-in" />
+                    )}
+                />
+                <Route path='sign-in' element={<SignIn />} />
+                <Route path='my-goals' element={<MyGoals />} />
+                <Route path='buddy-goals' element={<BuddyGoals />} />
+                <Route path='set-goals' element={<SetGoals />} />
+                <Route path='history' element={<History />} />
+                <Route path='settings' element={<Settings />} />
+            </Routes>
         </DialogProvider>
     )
 }
